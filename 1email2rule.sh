68e9368c64ebb924e000cc2d90376f67d13e177d
Reemus <millsgold@pm.me>
2022-10-27 10:06:46 -0600

    initial email sorting script

---
+++ 1email2rule.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# print usage if no file is provided
+if [ $# -ne 1 ] 
+then 
+echo 'Usage: This script takes a file of email lists and divides duplicate
+        email lines into separate files to prevent account lockout in 
+        brute force attacks'
+else
+echo 'todo'
+fi
